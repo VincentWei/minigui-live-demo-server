937b1252d4facddac37a4a8e87b632a14d762571
Vincent Wei <vincent@minigui.org>
2020-11-23 11:39:45 +0800

    use build-minigui-5.0

--- src/wdserver.c
+++ src/wdserver.c
@@ -167,7 +167,7 @@ static struct _demo_info {
     char* const def_mode;
 } _demo_list [] = {
     {"mguxdemo", "/usr/local/bin/", "/usr/local/bin/mguxdemo", "360x480-16bpp"},
-    {"cbplusui", "/srv/devel/build-minigui-4.0/mg-demos/cbplusui/", "/srv/devel/build-minigui-4.0/mg-demos/cbplusui/cbplusui", "240x240-16bpp"},
+    {"cbplusui", "/srv/devel/build-minigui-5.0/mg-demos/cbplusui/", "/srv/devel/build-minigui-5.0/mg-demos/cbplusui/cbplusui", "240x240-16bpp"},
 };
 
 /* return 0: bad request;
